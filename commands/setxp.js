61fd93b81fc64325db051d02157c38dcf59b9796
Jules Decaestecker <38179298+Jovillios@users.noreply.github.com>
2019-04-16 14:21:06 +0200

    Update setxp.js

--- commands/setxp.js
+++ commands/setxp.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 let xp = require("../xp.json");
 
 module.exports.run = async (bot, message, args) => {
-	xp[message.author.id] = args;
+	xp[message.author.id].xp = args;
 }
 
 module.exports.help = {
